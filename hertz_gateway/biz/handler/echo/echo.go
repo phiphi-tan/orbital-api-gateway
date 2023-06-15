b1f630458a95c30f1acfb7507cb0690f1523f13d
delishad21 <soojohan@gmail.com>
2023-06-15 16:52:18 +0800

    Fixed parameter validation

--- hertz_gateway/biz/handler/echo/echo.go
+++ hertz_gateway/biz/handler/echo/echo.go
@@ -7,4 +7,26 @@ import (
 
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
+<<<<<<< Updated upstream
 )
+=======
+	clients "github.com/phiphi-tan/orbital-api-gateway/hertz_gateway/biz/clients"
+	echo "github.com/phiphi-tan/orbital-api-gateway/hertz_gateway/biz/model/echo"
+)
+
+// Echo .
+// @router /echo/echo [POST]
+func Echo(ctx context.Context, c *app.RequestContext) {
+
+	//Binding and Validation of Params
+	var err error
+	var req echo.Request
+	err = c.BindAndValidate(&req)
+	if err != nil {
+		c.String(consts.StatusBadRequest, err.Error())
+		return
+	}
+
+	clients.GenericCall("echo", "echo", ctx, c)
+}
+>>>>>>> Stashed changes
